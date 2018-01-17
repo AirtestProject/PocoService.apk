f9a21f16cb6d525de0aa82c641525b8383a8291b
lxn3032 <lxn3032@corp.netease.com>
2018-01-17 14:55:17 +0800

    auto background for this activity

--- pocoservice/src/main/java/com/netease/open/pocoservice/TestActivity.java
+++ pocoservice/src/main/java/com/netease/open/pocoservice/TestActivity.java
@@ -1,8 +1,14 @@
 package com.netease.open.pocoservice;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -15,6 +21,18 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("PocoService", "hahahahahah");
+        Log.i("PocoService", "onResume");
+
+        PackageManager pm = getPackageManager();
+        ResolveInfo homeInfo = pm.resolveActivity(new Intent(Intent.ACTION_MAIN)
+                .addCategory(Intent.CATEGORY_HOME), 0);
+
+        ActivityInfo ai = homeInfo.activityInfo;
+        Intent startIntent = new Intent(Intent.ACTION_MAIN);
+        startIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        startIntent.setComponent(new ComponentName(ai.packageName, ai.name));
+        startActivity(startIntent);
+
+        Toast.makeText(getApplicationContext(), "poco service is running!", Toast.LENGTH_SHORT).show();
     }
 }
